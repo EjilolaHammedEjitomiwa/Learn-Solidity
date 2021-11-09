96f4ca6df8a4d98c223033656fe69eadc2633c2d
EjilolaHammedEjitomiwa <geotechbrain@gmail.com>
2021-11-09 23:41:05 +0100

    Simple Storage

---
+++ SimpleStorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    
+    // can only accept positive integer
+    uint256 favouriteNumber;
+    
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    
+    People[] public people;
+    
+    mapping (string => uint256) public nameToFavouriteNumber;
+    
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+    
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+    
+    
+    function addPerson(uint256 _favouriteNumber , string memory _name) public {
+        people.push(People( _favouriteNumber,_name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+    
+    
+    
+    
+}
